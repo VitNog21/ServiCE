f5a3470d989c4992316d3502312d713e6330ca4f
João Gabriel <joaogabrielborges@alu.ufc.br>
2026-06-28 11:41:35 -0300

    chore: comment out Google OAuth buttons during redirect testing

--- service-frontend/src/pages/Login.jsx
+++ service-frontend/src/pages/Login.jsx
@@ -104,19 +104,12 @@ const Login = () => {
           </button>
         </form>
 
+        {/* TODO: Reativar Google OAuth quando o Supabase tiver a redirect URL configurada
         <div className="divider">ou</div>
-
-        {/* BOTÃO GOOGLE COM SVG OFICIAL */}
         <button type="button" className="btn-google" onClick={handleGoogleLogin} disabled={loading}>
-          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" style={{ width: '20px', height: '20px' }}>
-            <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
-            <path fill="#4285F4" d="M46.64 24.32c0-1.63-.15-3.26-.44-4.84H24v9.03h12.72c-.53 2.84-2.14 5.25-4.59 6.81l7.41 5.74c4.35-4.01 6.81-9.92 6.81-16.74z"></path>
-            <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
-            <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.41-5.74c-2.22 1.48-5.07 2.36-8.48 2.36-6.26 0-11.57-4.22-13.46-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
-            <path fill="none" d="M0 0h48v48H0z"></path>
-          </svg>
           Entrar com Google
         </button>
+        */}
 
         <p className="register-link">
           Não tem conta? <Link to="/cadastro">Cadastre-se aqui</Link>
